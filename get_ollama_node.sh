376cbe1408f5cbce001ed4533a2ccb835059b592
ipr_adm <ipr_adm@codon-slurm-login-04.ebi.ac.uk>
2025-09-29 10:33:20 +0100

    Add scripts

---
+++ get_ollama_node.sh
@@ -0,0 +1,14 @@
+# Cancel all the other ollama processes for that user
+squeue -u $USER | grep ollama | awk '{print $1}' | xargs -r scancel
+
+echo "Starting server.."
+jobid=$(sbatch --gres=gpu:"$1":"$2" --time="$3":00:00 ollama_server.sh $4 | awk '{print $4}')
+sleep 5
+
+echo "Waiting for node name and port.."
+node=$(scontrol show job "$jobid" | grep NodeList=codon | awk -F '=' '{print $2}')
+sleep 5
+
+# Get port where Ollama process is listening
+port=$(ssh "$node".ebi.ac.uk "lsof -i -P -n | grep ollama | awk '{print \$9}' | awk -F '*:' '{print \$2}'")
+echo "$node:$port"
---
+++ ollama_server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=ollama_job
+#SBATCH --mem=64GB
+#SBATCH --error=error.log
+#SBATCH --output=output.log
+
+cd /hps/nobackup/agb/interpro/embl_llm_metrics/ollama/bin
+
+# Receive requests from outside on an available port
+export OLLAMA_HOST=0.0.0.0:0
+
+# Set up cache where Ollama models are going to be stored
+export OLLAMA_MODELS=/hps/nobackup/agb/interpro/ollama_models
+
+# Start server and run model passed as argument
+./ollama serve &
+sleep 5
+./ollama run "$1" &
+wait
