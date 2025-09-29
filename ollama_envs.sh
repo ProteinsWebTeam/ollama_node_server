6ec18b9f6a2a21bcbc044ffa8e928c07966f4aad
ipr_adm <ipr_adm@codon-slurm-login-01.ebi.ac.uk>
2025-09-29 15:30:41 +0100

    user-agnostic and env vars

---
+++ ollama_envs.sh
@@ -0,0 +1,4 @@
+# Receive requests from outside on an available port
+export OLLAMA_HOST=0.0.0.0:0
+export OLLAMA_BINARY=/hps/software/users/agb/interpro/llms/ollama/bin
+export OLLAMA_MODELS=/hps/nobackup/agb/interpro/ollama_models
--- ollama_server.sh
+++ ollama_server.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=ollama_job
 #SBATCH --mem=64GB
-#SBATCH --error=error.log
-#SBATCH --output=output.log
 
-cd /hps/software/users/agb/interpro/llms/ollama/bin 
-
-# Receive requests from outside on an available port
-export OLLAMA_HOST=0.0.0.0:0
-
-# Set up cache where Ollama models are going to be stored
-export OLLAMA_MODELS=/hps/nobackup/agb/interpro/ollama_models
+# Get Ollama binary and cache folder
+source ollama_envs.sh
 
 # Start server and run model passed as argument
 ./ollama serve &
