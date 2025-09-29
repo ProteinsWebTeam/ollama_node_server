e85127dcf507676fc06beb60066b1ed2684f50b1
ipr_adm <ipr_adm@codon-slurm-login-01.ebi.ac.uk>
2025-09-29 15:34:05 +0100

    Comment

--- ollama_envs.sh
+++ ollama_envs.sh
@@ -1,4 +1,6 @@
 # Receive requests from outside on an available port
 export OLLAMA_HOST=0.0.0.0:0
+
+# Env variables
 export OLLAMA_BINARY=/hps/software/users/agb/interpro/llms/ollama/bin
 export OLLAMA_MODELS=/hps/nobackup/agb/interpro/ollama_models
